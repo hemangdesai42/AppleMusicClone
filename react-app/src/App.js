cc991fe6dd946ce81e2ef8c90ab8f147dee91aa6
Hemang Desai <hmang42@icloud.com>
2021-05-11 20:16:31 -0400

    got rid of / route

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -81,9 +81,9 @@ function App() {
         <ProtectedRoute path="/users/:userId" exact={true} >
           <User />
         </ProtectedRoute>
-        <ProtectedRoute path="/" exact={true}>
+        {/* <ProtectedRoute path="/" exact={true}>
           <h1>My Home Page</h1>
-        </ProtectedRoute>
+        </ProtectedRoute> */}
       </Switch>
     </BrowserRouter>
   );
